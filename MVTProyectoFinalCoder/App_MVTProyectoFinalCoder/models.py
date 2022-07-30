0c44e2d64b62d473e1a5127c55b0f4db5104d255
SteveAraya <stevenaraya.24@gmail.com>
2022-07-30 12:52:41 -0600

    Trabajando con los modelos

--- MVTProyectoFinalCoder/App_MVTProyectoFinalCoder/models.py
+++ MVTProyectoFinalCoder/App_MVTProyectoFinalCoder/models.py
@@ -1,3 +1,25 @@
+from email.message import EmailMessage
+from itertools import pairwise
+from mailbox import NoSuchMailboxError
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    Nombre
+    Apellido
+    Email
+    Pais
+
+class Curso(models.Model):
+    Nombre
+    Descripcion
+    FechaInicio
+    FechaFin
+
+class Profesor(models.Model):
+    Nombre
+    Apellido
+    Email
+    Pais
+    Profesion
