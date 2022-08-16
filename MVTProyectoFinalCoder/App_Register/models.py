a616917213724fd7c497895f0a16c06a8407ae03
SteveAraya <stevenaraya.24@gmail.com>
2022-08-15 22:29:43 -0600

    Trabajando el la vista de registro

--- MVTProyectoFinalCoder/App_Register/models.py
+++ MVTProyectoFinalCoder/App_Register/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    tipo               = models.CharField(max_length=50)
+    marca              = models.CharField(max_length=50)
+    modelo             = models.CharField(max_length=50)
+    placa              = models.CharField(max_length=50)
+    annio              = models.IntegerField()
+    tipo_caja          = models.CharField(max_length=50)
+    combustible        = models.CharField(max_length=50)
+    cantidad_pasajeros = models.IntegerField()
+    numero_puertas     = models.IntegerField()
