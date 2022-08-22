360d98afd17515cb725430d643f33731aa3516ea
SteveAraya <stevenaraya.24@gmail.com>
2022-08-21 22:13:10 -0600

    Trabajando en la vista de About Us

--- MVTProyectoFinalCoder/App_MVTProyectoFinalCoder/views.py
+++ MVTProyectoFinalCoder/App_MVTProyectoFinalCoder/views.py
@@ -9,8 +9,25 @@ from django.contrib       import messages
 def Home(self):
     return render(self, "Home.html")
 
-def AboutUs(self):
-    return render(self, "AboutUs.html")
+def AboutUs(request):
+    history_steve  = False
+    history_adrian = False
+    if request.GET.get('view_history_steve'):
+        history_steve = True
+        context = {'history_steve': history_steve, 'history_adrian': history_adrian}
+    elif request.GET.get('close_history_steve'):
+        history_steve = False
+        context = {'history_steve': history_steve, 'history_adrian': history_adrian}
+    elif request.GET.get('view_history_adrian'):
+        history_adrian = True
+        context = {'history_adrian': history_adrian, 'history_steve': history_steve,}
+    elif request.GET.get('close_history_adrian'):
+        history_adrian = False
+        context = {'history_adrian': history_adrian, 'history_steve': history_steve,}
+    else:
+        context = {'history_steve': history_steve, 'history_adrian': history_adrian}
+
+    return render(request, "AboutUs.html", context)
 
 def AdminSite(self):
     return render(self, "Admin.html")
