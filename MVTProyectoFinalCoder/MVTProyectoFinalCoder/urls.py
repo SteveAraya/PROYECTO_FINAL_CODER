7671e4bf60aef5f566ca429645a25a17253bfddf
Steve Araya Solórzano <stevenaraya.24@gmail.com>
2022-08-23 19:52:32 -0600

    Trabajando en la vista Page Not Found

--- MVTProyectoFinalCoder/MVTProyectoFinalCoder/urls.py
+++ MVTProyectoFinalCoder/MVTProyectoFinalCoder/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +16,7 @@ urlpatterns = [
     path('register/', include("App_Register.urls")),
     path('reservation/', include("App_Reservation.urls")),
 ]
+
+handler404 = 'App_MVTProyectoFinalCoder.views.PageNotFound'
+
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
